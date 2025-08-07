bf4e4ff1bf961ddb3c95f0e345fc43f2e2c25555
omariksmith <o_smith5544@sbcglobal.net>
2025-08-07 00:00:19 -0500

    Arrays4

--- src/main/java/org/javaclass/arrays/exercises/DeclareArrays.java
+++ src/main/java/org/javaclass/arrays/exercises/DeclareArrays.java
@@ -1,11 +1,11 @@
 package org.javaclass.arrays.exercises;
-//! 1. Declare an char array explicit initialized with the characters h,e,l,l,o.
+//! 1. Declare a char array explicit initialized with the characters h,e,l,l,o.
 
 //! 2. Declare an int literal initialized array with a size of the first array.
 
 //! 3. Declare a String array explicit initialized with "zero","one", "two".
 
-//! 4. Declare an String literal initialized array with a size of the first String array.
+//! 4. Declare a String literal initialized array with a size of the first String array.
 
 //! 5. Initialize getters for this class.
 
--- src/main/java/org/javaclass/arrays/solutions/Declare2DArray.java
+++ src/main/java/org/javaclass/arrays/solutions/Declare2DArray.java
@@ -1,12 +1,12 @@
 package org.javaclass.arrays.solutions;
 
-//! 1. Declare an char 2d array explicit initialized with the characters {h,e,l,l,o},{w,o,r,l,d,s}.
+//! 1. Declare a char 2d array explicit initialized with the characters {h,e,l,l,o},{w,o,r,l,d,s}.
 
 //! 2. Declare an int literal initialized 2d array with a size of the first 2d array.
 
 //! 3. Declare a String 2d array explicit initialized with {"zero","one", "two"},{"three","four","five"}
 
-//! 4. Declare an String literal initialized 2d array with a size of the first 2d String array.
+//! 4. Declare a String literal initialized 2d array with a size of the first 2d String array.
 
 //! 5. Initialize getters for this class.
 
